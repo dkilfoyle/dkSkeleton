fe6b446ad9eefd8b92b940f9563ab9331b8698da
Dean <dkilfoyle@xtra.co.nz>
2014-09-30 22:36:34 +1300

    use iris as demo data

--- loadData.r
+++ loadData.r
@@ -2,11 +2,15 @@ require(dplyr)
 require(tidyr)
 require(lubridate)
 
-mydata = read.csv("data/project.csv", 
-                  na.strings="NA",
-                  stringsAsFactors=T,
-                  colClasses = c("mydate"="character"))
+mydata = iris %>%
+  mutate(numeric1 = Sepal.Length, numeric2 = Sepal.Width, factor1 = Species) # TODO: use rename with dplyr 0.4
+mydata$factor1 = factor(replace(mydata$factor1, mydata$factor1=="virginica", "versicolor"))
 
-mydata = mydata %>%
-  mutate(myDate = as.Date(myDate, format="%d/%m/%Y"),
-         myDateTime = as.POSIXct(myDateTime, format="%d/%m/%Y %H:%M", tz=""))
+# mydata = read.csv("data/project.csv", 
+#                   na.strings="NA",
+#                   stringsAsFactors=T,
+#                   colClasses = c("mydate"="character"))
+# 
+# mydata = mydata %>%
+#   mutate(myDate = as.Date(myDate, format="%d/%m/%Y"),
+#          myDateTime = as.POSIXct(myDateTime, format="%d/%m/%Y %H:%M", tz=""))
